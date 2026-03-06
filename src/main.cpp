93800e2c8c2aea66314ba510ae1c3cb00d8331ca
bfmhno3 <858446559@qq.com>
2026-03-06 22:55:10 +0800

    feat: 启用高 DPI 设置并使用高 DPI 图标

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "main.hpp"
 
 #include <QApplication>
+#include <QGuiApplication>
 
 #include "gui/main_window.hpp"
 
 int main(int argc, char* argv[]) {
+  // 启用高 DPI 设置
+  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  // 使用高 DPI 图标/像素图
+  QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
   QApplication app(argc, argv);
 
   res::MainWindow window;
