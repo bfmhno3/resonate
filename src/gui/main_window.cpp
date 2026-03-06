71649d98dbb5e328eebf2af54a4c19538c31f536
bfmhno3 <858446559@qq.com>
2026-03-06 23:12:45 +0800

    feat: 实现窗口无边框

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -1,11 +1,16 @@
 #include "gui/main_window.hpp"
 
+#include <QWidget>
+
 #include "ui_main_window.h"
 
 namespace res {
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+
+  // 移除窗口边框和标题栏
+  setWindowFlags(Qt::FramelessWindowHint);
 }
 
 MainWindow::~MainWindow() { delete ui; }
