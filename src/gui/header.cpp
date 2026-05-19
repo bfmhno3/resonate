5258b0e2c8ed111cc81f22357710cb5e55b527aa
bfmhno3 <858446559@qq.com>
2026-05-19 13:08:54 +0800

    feat: add icon buttons and avatar to header component

--- src/gui/header.cpp
+++ src/gui/header.cpp
@@ -1,17 +1,34 @@
-//
-// Created by amara on 3/8/2026.
-//
+#include "gui/header.hpp"
 
-// You may need to build the project (run Qt uic code generator) to get
-// "ui_Header.h" resolved
-
-#include "../../include/gui/header.hpp"
+#include <QIcon>
+#include <QPixmap>
 
 #include "ui_Header.h"
 
 namespace res {
 Header::Header(QWidget *parent) : QWidget(parent), ui(new Ui::Header) {
   ui->setupUi(this);
+
+  auto setup_icon_button = [](QPushButton *button, const char *icon_path,
+                              const int icon_size) {
+    button->setText("");
+    button->setIcon(QIcon(icon_path));
+    button->setIconSize(QSize(icon_size, icon_size));
+  };
+
+  setup_icon_button(ui->btn_back, ":/assets/icons/header/back.svg", 18);
+  setup_icon_button(ui->btn_forward, ":/assets/icons/header/forward.svg", 18);
+  setup_icon_button(ui->btn_refresh, ":/assets/icons/header/refresh.svg", 18);
+  setup_icon_button(ui->btn_settings, ":/assets/icons/header/settings.svg", 18);
+
+  ui->line_edit_search->addAction(QIcon(":/assets/icons/header/search.svg"),
+                                  QLineEdit::LeadingPosition);
+
+  ui->label_avatar->setText("");
+  const QPixmap avatar_pixmap(":/assets/icons/header/User.svg");
+  ui->label_avatar->setPixmap(
+      avatar_pixmap.scaled(20, 20, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+  ui->label_avatar->setAlignment(Qt::AlignCenter);
 }
 
 Header::~Header() { delete ui; }
